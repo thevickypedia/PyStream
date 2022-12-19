ee2d1c01b09bfb93311b90ca2cf3b2be363f8cc1
thevickypedia <vignesh.sivanandarao@gmail.com>
2022-12-18 19:08:08 -0600

    Handle streaming in a more reliable way Update requirements.txt

--- fast.py
+++ fast.py
@@ -2,11 +2,11 @@ import logging
 import mimetypes
 import os
 import warnings
-from typing import Optional
+from typing import Optional, BinaryIO, Tuple, Union, ByteString, AsyncIterable
 
-from fastapi import Cookie, Depends, FastAPI, Header, Request, Response
+from fastapi import Cookie, Depends, FastAPI, Header, Request, Response, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse, HTMLResponse, RedirectResponse
+from fastapi.responses import FileResponse, HTMLResponse, RedirectResponse, StreamingResponse
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from fastapi.templating import Jinja2Templates
 from starlette.middleware.sessions import SessionMiddleware
@@ -17,15 +17,9 @@ from models.settings import verify_auth
 
 logger = logging.getLogger(name="uvicorn.default")
 
-if not os.path.isfile(env.video_file):
-    raise FileNotFoundError(
-        f"{env.video_file} does not exist."
-    )
-if not (media_type := mimetypes.guess_type(env.video_file, strict=True)[0]):
-    warnings.warn(
-        message=f"Unable to guess the media type for {env.video_file}. Using 'video/mp4' instead."
-    )
-    media_type = "video/mp4"
+if not (content_type := mimetypes.guess_type(env.video_file, strict=True)[0]):
+    warnings.warn(message=f"Unable to guess the media type for {env.video_file}. Using 'video/mp4' instead.")
+    content_type = "video/mp4"
 
 app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key=settings.SESSION_TOKEN)
@@ -116,11 +110,100 @@ async def login(request: Request,
     return response
 
 
+def send_bytes_range_requests(file_obj: BinaryIO,
+                              start: int, end: int) -> AsyncIterable[Union[str, ByteString]]:
+    """Send a file in chunks using Range Requests specification RFC7233.
+
+    Args:
+        file_obj: File data as bytes.
+        start: Start of range.
+        end: End of range.
+
+    Yields:
+        ByteString:
+        Bytes as iterable.
+    """
+    with file_obj as stream:
+        stream.seek(start)
+        while (pos := stream.tell()) <= end:
+            read_size = min(settings.CHUNK_SIZE, end + 1 - pos)
+            yield stream.read(read_size)
+
+
+def _get_range_header(range_header: str,
+                      file_size: int) -> Tuple[int, int]:
+    """Proces range header.
+
+    Args:
+        range_header: Range values from the headers.
+        file_size: Size of the file.
+
+    Returns:
+        Tuple:
+        Start and end of the video file.
+    """
+    _invalid_range = HTTPException(
+        status_code=status.HTTP_416_REQUESTED_RANGE_NOT_SATISFIABLE,
+        detail=f"Invalid request range (Range:{range_header!r})",
+    )
+    try:
+        h = range_header.replace("bytes=", "").split("-")
+        start = int(h[0]) if h[0] != "" else 0
+        end = int(h[1]) if h[1] != "" else file_size - 1
+    except ValueError:
+        raise _invalid_range
+    if start > end or start < 0 or end > file_size - 1:
+        raise _invalid_range
+    return start, end
+
+
+def range_requests_response(range_header: str,
+                            file_path: str) -> StreamingResponse:
+    """Returns StreamingResponse using Range Requests of a given file.
+
+    Args:
+        range_header: Range values from the headers.
+        file_path: Path of the file.
+
+    Returns:
+        StreamingResponse:
+        Streaming response from fastapi.
+    """
+    file_size = os.stat(file_path).st_size
+    headers = {
+        "content-type": content_type,
+        "accept-ranges": "bytes",
+        "content-encoding": "identity",
+        "content-length": file_size,
+        "access-control-expose-headers": (
+            "content-type, accept-ranges, content-length, "
+            "content-range, content-encoding"
+        ),
+    }
+    start = 0
+    end = file_size - 1
+    status_code = status.HTTP_200_OK
+
+    if range_header is not None:
+        start, end = _get_range_header(range_header=range_header, file_size=file_size)
+        size = end - start + 1
+        headers["content-length"] = str(size)
+        headers["content-range"] = f"bytes {start}-{end}/{file_size}"
+        status_code = status.HTTP_206_PARTIAL_CONTENT
+
+    return StreamingResponse(
+        content=send_bytes_range_requests(open(file_path, mode="rb"), start, end),
+        headers=headers,
+        status_code=status_code,
+    )
+
+
 # noinspection PyShadowingBuiltins
 @app.get("/video")
 async def video_endpoint(request: Request, range: Optional[str] = Header(None),
                          session_token: Optional[str] = Cookie(None),
-                         credentials: HTTPBasicCredentials = Depends(security)) -> Response:
+                         credentials: HTTPBasicCredentials = Depends(security)) -> Union[RedirectResponse,
+                                                                                         StreamingResponse]:
     """Opens the video file to stream the content.
 
     Args:
@@ -145,18 +228,6 @@ async def video_endpoint(request: Request, range: Optional[str] = Header(None),
             logger.info(f"Connection received from {request.client.host} via {host}")
         if ua := request.headers.get('user-agent'):
             logger.info(f"User agent: {ua}")
-    start, end = range.replace("bytes=", "").split("-")
-    start = int(start)
-    end = int(end) if end else start + settings.CHUNK_SIZE
-    with open(env.video_file, "rb") as video:
-        video.seek(start)
-        if "chrome" in request.headers.get("sec-ch-ua", "NO_MATCH").lower():
-            data = video.read()
-        else:
-            data = video.read(end - start)
-        file_size = str(env.video_file.stat().st_size)
-        headers = {
-            'Content-Range': f'bytes {str(start)}-{str(end)}/{file_size}',
-            'Accept-Ranges': 'bytes'
-        }
-        return Response(content=data, status_code=206, headers=headers, media_type=media_type)
+    return range_requests_response(
+        range_header=range, file_path=env.video_file
+    )
